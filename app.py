6396392c4c353698f010050334026fbd17a47bc6
AlanLatte <shukurovfoteh@gmail.com>
2021-03-17 23:57:18 +0000

    [+] init for deploy

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def vk():
     return redirect('https://vk.com/alan.latte')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)), debug=True)
+    app.run(host="0.0.0.0")
